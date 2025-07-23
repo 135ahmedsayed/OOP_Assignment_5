09fdc80f903e1189ba455082a93230e6b4aed36b
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-23 22:48:36 +0300

    resolve FirstProject(1,2)

--- OOP_Assignment_05/HelperClass.cs
+++ OOP_Assignment_05/HelperClass.cs
@@ -1,2 +1,27 @@
 ﻿namespace OOP_Assignment_05;
-
+#region FirstProject
+public class Point3D
+{
+    #region FirstProject(1,2)
+    public int p1 { get; set; }
+    public int p2 { get; set; }
+    public int p3 { get; set; }
+    private Point3D() : this(0, 0, 0) { } // chaining in constructors
+    public Point3D(int x, int y)// chaining in constructors
+    {
+        this.p1 = x;
+        this.p2 = y;
+    }
+    public Point3D(int p1, int p2, int p3)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+        this.p3 = p3;
+    }
+    public override string ToString()
+    {
+        return $"Point Coordinates: ({p1}, {p2}, {p3})";
+    }
+    #endregion
+}
+#endregion
--- OOP_Assignment_05/Program.cs
+++ OOP_Assignment_05/Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            #region FirstProject
+            #region FirstProject(1,2)
+            Point3D p = new Point3D(10, 10, 10);
+            Console.WriteLine(p.ToString());
+            #endregion
+            #endregion
 
             Console.ReadKey();
         }
